f50e5d63865211a24053e477d59514b13821d1ff
Jiego Pasamanero <jiegobpasamanero@gmail.com>
2025-03-18 18:13:43 -0600

    Quick Update

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
 ﻿namespace Connect_Four_Final_Project_OOP_2025
 {
+    class ConnectFour()
+    {
+        //board is 7x6
+        private bool gameStart = false;
+        bool turn;
+        static List<ConnectFour> pieceLocations;
+        int myPlayThisTurn;
+
+
+        public Player()
+        {
+
+        }
+        public PlayATurn(int myPlayMyTurn)
+        {
+            
+        }
+
+        public DrawBoard()
+        {
+
+        }
+
+        public ShowThePieces() : DrawBoard()
+        {
+
+        }
+
+
+    }
+
+    class PlayerFunctions
+    {
+        ConnectFour Player1;
+
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConnectFour game = new ConnectFour();
+
+            game
         }
     }
 }
